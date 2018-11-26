fbc849492e118237d4c46a2e19d1fa1b81bef0da
mariahmval <mariahm@pm.me>
2018-11-26 09:46:00 +0100

    Creado CVector y VectorTest en MonoDevelop y algoritmo búsqueda binaria.

---
+++ CVector/CVector/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CVector
+{
+    class Vector
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int BinarySearch(int[] v, int x) {
+
+            int menor = 0;
+            int mayor;
+            int posicionActiva = v.Length / 2;
+
+            if (v.Length > 0)
+                mayor = v.Length - 1;
+            else
+                return -1;
+
+            while (v[posicionActiva] != x && mayor > menor) {
+                if (v[posicionActiva] > x)
+                    mayor = posicionActiva - 1;
+                else
+                    menor = posicionActiva + 1;
+
+                posicionActiva = (mayor - menor) / 2 + menor;
+            }
+
+            if (v[posicionActiva] == x)
+                return posicionActiva;
+            else
+                return -1;
+        }
+
+    }
+}
---
+++ CVector/CVector/VectorTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+namespace CVector
+{
+    [TestFixture]
+    public class VectorTest
+    {
+        [Test]
+        public void BinarySearch()
+        {
+            Assert.AreEqual(-1, Vector.BinarySearch(new int[] {45}, 20));
+            Assert.AreEqual(2, Vector.BinarySearch(new int[] {1,5,15,30}, 15));
+            Assert.AreEqual(0, Vector.BinarySearch(new int[] {10,15}, 10));
+            Assert.AreEqual(1, Vector.BinarySearch(new int[] {10,15}, 15));
+            Assert.AreEqual(2, Vector.BinarySearch(new int[] {1,1,1,1,1}, 1));
+            Assert.AreEqual(-1, Vector.BinarySearch(new int[] { 3, 7, 12, 20, 70, 85 }, 2));
+            Assert.AreEqual(0, Vector.BinarySearch(new int[] {3,7,12,20,70,85}, 3));
+            Assert.AreEqual(3, Vector.BinarySearch(new int[] {3,7,12,20,70,85}, 20));
+            Assert.AreEqual(5, Vector.BinarySearch(new int[] {3,7,12,20,70,85}, 85));
+            Assert.AreEqual(-1, Vector.BinarySearch(new int[] {3,7,12,20,70,85}, 86));
+        }
+
+        [Test]
+        public void BinarySearchEmpty()
+        {
+            Assert.AreEqual(-1, Vector.BinarySearch(new int[] { }, 20));
+        }
+
+    }
+}
