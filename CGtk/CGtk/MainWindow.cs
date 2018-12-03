788d93369b87eb45e1f41634b108cf0ce3d257f7
María Homsani <1dam.mhomsani@institutoserpis.org>
2018-12-03 09:49:02 +0100

    Creado CGtk con interfaz gráfica en la que introduces un nombre y devuelve un saludo con dicho nombre.

---
+++ CGtk/CGtk/MainWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        //buttonAceptar.Clicked += delegate {
+        //    string nombre = entryNombre.Text;
+        //    labelSaludo.Text = "Hola " + nombre;
+        //    Console.WriteLine("Click en buttonAceptar con nombre=" + nombre);
+        //};
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnButtonAceptarClicked(object sender, EventArgs e)
+    {
+        //string nombre = entryNombre.Text;
+        labelSaludo.Text = "Hola " +entryNombre.Text;
+        //Console.WriteLine("Click en buttonAceptar con nombre=" +nombre);
+    }
+}
