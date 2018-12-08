5678aab0efd5a9ebf6fd359fcbf3ba312d5f1df4
María Homsani <1dam.mhomsani@institutoserpis.org>
2018-12-08 13:31:31 +0100

    Añadido CSumaGtk, programa para operaciones simples entre dos números.

---
+++ CSumaGtk/CSumaGtk/MainWindow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnButtonSumaClicked(object sender, EventArgs e)
+    {
+        float num1 = float.Parse(entryNumero1.Text);
+        float num2 = float.Parse(entryNumero2.Text);
+        float suma = num1 + num2;
+        labelResultado.Text = "" +suma;
+    }
+
+    protected void OnButtonMultiplicaClicked(object sender, EventArgs e)
+    {
+        float num1 = float.Parse(entryNumero1.Text);
+        float num2 = float.Parse(entryNumero2.Text);
+        float multiplicacion = num1 * num2;
+        labelResultado.Text = "" +multiplicacion;
+    }
+
+    protected void OnButtonRestaClicked(object sender, EventArgs e)
+    {
+        float num1 = float.Parse(entryNumero1.Text);
+        float num2 = float.Parse(entryNumero2.Text);
+        float resta = num1 - num2;
+        labelResultado.Text = "" +resta;
+    }
+
+    protected void OnButtonDividirClicked(object sender, EventArgs e)
+    {
+        float num1 = float.Parse(entryNumero1.Text);
+        float num2 = float.Parse(entryNumero2.Text);
+        float division = num1 / num2;
+        labelResultado.Text = "" +division;
+    }
+}
