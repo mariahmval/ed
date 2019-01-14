16b4fe35aa4ee724788d0932c03ea17ea4ab90c5
María Homsani <1dam.mhomsani@institutoserpis.org>
2019-01-14 09:43:17 +0100

    Añadido CWindow, creado con el objetivo de trastear con ventanas emergentes.

---
+++ CWindow/CWindow/MainWindow.cs
@@ -0,0 +1,27 @@
+﻿using CWindow;
+using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        Title = "Ventana principal";
+
+        button.Clicked += delegate {
+            //if (WindowHelper.Confirm(this, "¿Quieres tomar algo?"))
+            //Console.WriteLine("Ha dicho Yes");
+            //new MyWindow();
+            vBoxReset.Visible = !vBoxReset.Visible;
+        };
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
